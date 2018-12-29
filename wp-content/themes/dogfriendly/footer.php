efbb9dc0f8b30912e94a8cfeff32070dcfc058f2
Harsh Bhardwaj <Harsh.s.bhardwaj@gmail.com>
2018-12-29 00:58:34 -0800

    removing unwanted themes
    removing some unwanted themes from the project

--- wp-content/themes/dogfriendly/footer.php
+++ wp-content/themes/dogfriendly/footer.php
@@ -2,5 +2,6 @@
     <p>Just footer for the page</p>
 </footer>
     
+<?php wp_footer(); ?>
     </body>
 </html>
--- wp-content/themes/dogfriendly/functions.php
+++ wp-content/themes/dogfriendly/functions.php
@@ -2,7 +2,10 @@
 
 function dogfr_script_enqueue() {
 
-    wp_enqueue_style('customstyle', get_template_directory_uri().'css/dogfr.css', array(), '1.0.0', 'all' ); 
+    wp_enqueue_style('customstyle', get_template_directory_uri().'css/dogfr.css', array(), '1.0.0', 'all' );
+    wp_enqueue_script('customjs', get_template_directory_uri().'js/dogfr.js', array(), '1.0.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'dogfr_script_enqueue');
+
+add_theme_support('menus');
